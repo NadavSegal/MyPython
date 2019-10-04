20fae3d1e378930cff0e30d71ce309d2bbc37ba7
NadavSegal <na.segal@gmail.com>
2019-10-04 13:50:27 +0300

    update

--- DataAnalysis/Main.py
+++ DataAnalysis/Main.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 
 # inputs:
-DataPath = "C:/Users/naseg/Documents/MATLAB/DataAnalysis/Data/test_data.csv"
+DataPath = "C:/Users/Nadav/Documents/MATLAB/DataAnalysis/Data/test_data.csv"
 Q1 = '2019-07'
 Q2 = '2019-04'
 #Cross = ['web'; 'mobile_web';'utm_65';'utm_102';'medium_1';'medium_2';'medium_3';'medium_4'];
@@ -20,8 +20,20 @@ Qdates = Data.growth_policy_quote_level_created_quarter
 Data1 = Data[Qdates == Q1]
 Data2 = Data[Qdates == Q2]
 Delta = (np.sum(Data1.new_revenue)/np.sum(Data2.new_revenue)-1)*100
+headers1 = list(Data1.columns.values)
+
+for i in 1: len(Cross)
+    1
+
+mask1 = Data1[headers1].values == [Cross[0][0]]
+
+
+
 B= Data1[Data1.Product == 'product_1']
 
+#a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+#pd.DataFrame(d1.values[mask], d1.index[mask], d1.columns)
+#b = Data1.replace(to_replace ="utm_65",value =1)
 #Data.columns
 Table1 = pd.pivot_table(Data, 'new_revenue',['Product'],aggfunc = np.sum,margins = True)
 Table1.values[Table1.index == 'product_1']
